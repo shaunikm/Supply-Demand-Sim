82a446ad065f377e48f052742ed6ff3ad2f1f27e
Shaunik Musukula <68877932+shaunikm@users.noreply.github.com>
2023-11-07 23:01:44 -0800

    add base math functions

---
+++ math_func.cpp
@@ -0,0 +1,20 @@
+/**
+ * This file contains the functions to be used in main.py
+ * 
+ * @author Shaunik Musukula
+ * @version 1.1
+*/
+#include <iostream>
+#include <cmath>
+
+extern "C" {
+    long* getRangeSquares(int start, int end) {
+        long* squares = new long[end-start+1];
+
+        for (long i=start; i < end+1; i++) {
+           squares[i - start] = pow(i, 2);
+        }
+
+        return squares;
+    }
+}
