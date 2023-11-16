1402a279d19303c62d6e4ad5edff1f99f394e4a1
Shaunik Musukula <68877932+shaunikm@users.noreply.github.com>
2023-11-16 14:09:19 -0800

    array to list splicing

--- Windows/c_func.py
+++ Windows/c_func.py
@@ -2,6 +2,6 @@ import c_loader as cl
 
 def getRangeSquares(start, end):
     squares = cl.cpp_file.getRangeSquares(start, end)
-    result = [squares[i] for i in range(end-start+1)]
+    result = squares[:end-start]
     del squares
     return result
